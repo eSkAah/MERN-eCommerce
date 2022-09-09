be96eb3886a0e96cf88fcaf612a4b7535f53b463
eSkAah <allan.breuil@hotmail.fr>
2022-09-09 11:21:24 +0200

    Section3_end_added_env

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -16,4 +19,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(8080, console.log("Server started on port 8080"));
+const PORT = process.env.PORT || 8080;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
